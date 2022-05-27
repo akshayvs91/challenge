97e7ca9da8850614c325081b780d9ba50ae502a4
akshayvs91 <57113555+akshayvs91@users.noreply.github.com>
2022-05-27 16:59:44 +0530

    Create variables.tf

---
+++ terraform_aws/variables.tf
@@ -0,0 +1,12 @@
+variable "ec2_ami" {
+  type = string
+  desciption = "Specify the AMI id to launch instance"
+
+
+variable "ec2_instance_type" {
+  type = string
+  desciption = "Specify the instance type of the instance"
+
+variable "aws_region" {
+  type = string
+  desciption = "Specify the AWS region"
